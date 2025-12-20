74c2585da07a721967c4d9b0db1df6c4106710d2
Alexandru Georgescu <alexandru.georgescu@gmail.com>
2025-12-20 08:26:17 +0200

    Deployment scripts.

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,13 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
+nvm use 18
 echo "🛠  Building Docusaurus site..."
-
 npm run build
 
-echo "🚀 Publishing to GitHub Pages (gh-pages branch)..."
+tar -cvf docs.tar -C ./build .
+echo "🚀 Publishing to docs.datanexions.com"
+scp docs.tar docs.datanexions.com:/tmp/
 
-npx gh-pages -d build -b gh-pages
+ssh docs.datanexions.com "
+set -e
+sudo systemctl stop streamsf
+echo "Removing old documentation files..."
+sudo rm -rf /usr/share/nginx/docusaurus/*
+echo "Extracting documentation archive..."
+sudo tar --warning=no-unknown-keyword -xf /tmp/docs.tar -C /usr/share/nginx/docusaurus
+sudo chown -R root:root /usr/share/nginx/docusaurus
+echo "Starting nginx service..."
+sudo systemctl restart nginx
+"
 
-echo "✅ Done. Site published at: https://datanexions.github.io/documentation/"
+echo "✅ Done. Site published at: https://docs.datanexions.com"
