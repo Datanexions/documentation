1b4067e4063d0ff6328c005a8383988dd394d361
emericpineau <epu@datanexions.com>
2026-01-21 11:33:42 +0100

    wip

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,9 +46,6 @@ const config = {
         docs: {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Datanexions/documentation/tree/main/',
         },
         blog: {
           showReadingTime: true,
